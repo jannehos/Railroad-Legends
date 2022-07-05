adeed7fa7331cec58e47a1f7d2839eaea2f352aa
niemha3 <harri.nieminen22@gmail.com>
2022-07-05 09:20:47 +0300

    api-pohja lisätty

--- Railroad_Legends/Program.cs
+++ Railroad_Legends/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace Railroad_Legends
 {
@@ -28,4 +33,50 @@ namespace Railroad_Legends
 
 
     }
+
+
+    public static class ApiHelper
+    {
+        // create HTTP client
+        private static HttpClient GetHttpClient(string url)
+        {
+            var client = new HttpClient { BaseAddress = new Uri(url) };
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return client;
+        }
+
+        public static async Task<T> RunAsync<T>(string url, string urlParams)
+        {
+            try
+            {
+                using (var client = GetHttpClient(url))
+                {
+                    // send GET request
+                    HttpResponseMessage response = await client.GetAsync(urlParams);
+
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        var json = await response.Content.ReadAsStringAsync();
+
+                        // JSON to an object
+                        var result = JsonSerializer.Deserialize<T>(json);
+                        return result;
+                    }
+
+                    return default(T);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return default(T);
+            }
+
+
+
+        }
+
+    }
+
 }
