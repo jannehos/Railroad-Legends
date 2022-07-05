7690477e291ffda1dc51a284632fdb6750c5b126
Jepsuli <hanninen.jerejohannes@gmail.com>
2022-07-05 10:22:46 +0300

    tekee jotain

--- Railroad_Legends/Program.cs
+++ Railroad_Legends/Program.cs
@@ -9,64 +9,87 @@ namespace Railroad_Legends
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
 
 
             Console.WriteLine("Hello World!"); // Lauran kommentti
+            await GetTrainInfo();
+         
+        }
+        public static async Task GetTrainInfo()
+        {
+            Class1 klassi = new Class1();
+            Console.Write("\nGive name of the fruit: ");
+            int input = Convert.ToInt32(Console.ReadLine());
 
+            Class1 juna = await JunaApi.GetTrain(input);
+
+            if (juna == null)
+                Console.WriteLine("\n  Fruit not found :(");
+            else
+                Console.WriteLine(klassi.trainNumber);
 
         }
-    }
-    public static class JunaApi
-    {
-        const string url = "https://rata.digitraffic.fi/infra-api/";
-      
-    }
-
-
-    public static class ApiHelper
-    {
-        // create HTTP client
-        private static HttpClient GetHttpClient(string url)
+        public static class JunaApi
         {
-            var client = new HttpClient { BaseAddress = new Uri(url) };
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            return client;
-        }
-
-        public static async Task<T> RunAsync<T>(string url, string urlParams)
-        {
-            try
+            const string url = "https://rata.digitraffic.fi/api/v1/trains/latest/";
+            public static async Task<Class1> GetTrain(int trainNumber)
             {
-                using (var client = GetHttpClient(url))
+                string urlParams = "" + trainNumber;
+
+                Class1 response = await ApiHelper.RunAsync<Class1>(url, urlParams);
+
+                return response;
+            }
+        }
+   
+   
+
+
+        public static class ApiHelper
+        {
+            // create HTTP client
+            private static HttpClient GetHttpClient(string url)
+            {
+                var client = new HttpClient { BaseAddress = new Uri(url) };
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                return client;
+            }
+
+            public static async Task<T> RunAsync<T>(string url, string urlParams)
+            {
+                try
                 {
-                    // send GET request
-                    HttpResponseMessage response = await client.GetAsync(urlParams);
-
-                    if (response.StatusCode == HttpStatusCode.OK)
+                    using (var client = GetHttpClient(url))
                     {
-                        var json = await response.Content.ReadAsStringAsync();
+                        // send GET request
+                        HttpResponseMessage response = await client.GetAsync(urlParams);
 
-                        // JSON to an object
-                        var result = JsonSerializer.Deserialize<T>(json);
-                        return result;
+                        if (response.StatusCode == HttpStatusCode.OK)
+                        {
+                            var json = await response.Content.ReadAsStringAsync();
+
+                            // JSON to an object
+                            var result = JsonSerializer.Deserialize<T>(json);
+                            return result;
+                        }
+
+                        return default(T);
                     }
-
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
                     return default(T);
                 }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-                return default(T);
-            }
 
 
+
+            }
 
         }
 
     }
-
 }
