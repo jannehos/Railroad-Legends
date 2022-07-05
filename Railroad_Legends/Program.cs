f284447648abaa1bb9c9eb53d6aff40b36d82816
Jepsuli <hanninen.jerejohannes@gmail.com>
2022-07-05 09:23:09 +0300

    turha

--- Railroad_Legends/Program.cs
+++ Railroad_Legends/Program.cs
@@ -6,14 +6,14 @@ namespace Railroad_Legends
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
+
             Console.WriteLine("Hello World!"); // Lauran kommentti
-=======
+
             Console.WriteLine("Hello World!");
 
             //Jere mukana
             // Harrin kommentti 1ef844d26c059f63dcf3bcf5aef9857e6a1f1688
->>>>>>> 6ced71945c58db7c7cca2717878e9ca4914a2cd1
+
         }
     }
     class Junat
